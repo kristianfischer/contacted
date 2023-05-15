c8743391ad814184d446b88d1962d839af650de4
Kristian Fischer <kristianfischer29@gmail.com>
2023-05-14 20:28:19 -0400

    Firebase completed and Login UI started

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,75 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity  } from 'react-native';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import { Text, SafeAreaView, TextInput, Button, Image } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
-interface LoginProp {
-    auth: firebase.auth.Auth;
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyCjASomuifpgKwYpgOmXwuvDby-cBh_Wso",
+    authDomain: "contacted-b203c.firebaseapp.com",
+    projectId: "contacted-b203c",
+    storageBucket: "contacted-b203c.appspot.com",
+    messagingSenderId: "560930713469",
+    appId: "1:560930713469:web:0f7c2c6dd8923a0a56a614",
+    measurementId: "G-CSHHR0914L"
+};
 
-const Login: React.FC<LoginProp> = ({ auth }) => {
-        
-        const [email, setEmail] = useState("");
-        const [password, setPassword] = useState("");
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const SignUp = async () => {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
+                console.log("hi");
                 const user = userCredential.user;
-                console.log("BOOYAH");
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                console.log(errorMessage);
             });
     }
 
     const SignIn = async () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Signed in 
                 const user = userCredential.user;
+                console.log("success");
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                console.log(errorMessage);
             });
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <input
-                className="border border-gray-400 rounded-md p-2 w-full mb-4"
-                type="email"
+        <SafeAreaView>
+
+            <Text
+                className="text-6xl"
+            >
+                Login
+            </Text>
+
+
+            <TextInput
+                id="email"
+                className="w-full mt-1 border px-4 py-2 focus:border-gray-300"
                 placeholder="Email"
-                value= {email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                className="border border-gray-400 rounded-md p-2 w-full mb-4"
-                type="password"
+                onChangeText={setEmail}
+                value={email}
+                style={{padding: 10}}   
+            /> 
+            
+            <TextInput
+                id="password"
+                className="w-full mt-1 border px-4 py-2 focus:border-gray-300"
                 placeholder="Password"
-                value= {password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChangeText={setPassword}
+                value={password}
+                style={{padding: 10}}   
+            /> 
+
+            <Button
+                title="Login"
+                color="#f194ff"
+                onPress={SignIn}
+                />
+                
+            <Button
+                title="Not a Member? Register Here"
+                onPress={SignUp}
             />
-            <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                onClick={SignUp}
-            >
-                SignUp
-            </button>
-            <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                onClick={SignIn}
-            >
-                SignIn
-            </button>
-            </div>
+        </SafeAreaView>
     );
-}
+};
     
 export default Login;
